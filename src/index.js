c3596713b2ce42d7564af0c6f9bc4b05bc5e585e
probably-kit <probably.kit@gmail.com>
2024-01-08 17:21:32 +0100

    fix bugs and deploy gitpages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/main.css';
 
 function createTabNavigation() {
   const tabs = { home: homeTab, menu: menuTab, contact: contactTab };
-  const content = gitdocument.getElementById('content');
+  const content = document.getElementById('content');
 
   for (let tab in tabs) {
     const btn = document.createElement('button');
