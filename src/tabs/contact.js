5bfd92b160ac3415712a56a47c97d664f53b734c
probably-kit <probably.kit@gmail.com>
2024-01-08 16:40:07 +0100

    Updates

--- src/tabs/contact.js
+++
@@ -1,8 +0,0 @@
-function contactTab() {
-    const element = document.createElement('div');
-    element.innerHTML = '<h1>Welcome to contact</h1><p>Some description...</p>';
-    return element;
-  }
-  
-  export default contactTab;
-  
--- src/tabs/menu.js
+++
@@ -1,8 +0,0 @@
-function menuTab() {
-    const element = document.createElement('div');
-    element.innerHTML = '<h1>Welcome to Menu</h1><p>Some description...</p>';
-    return element;
-  }
-  
-  export default menuTab;
-  
