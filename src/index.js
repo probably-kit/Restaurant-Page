bfe9eab8ce236be9c40735be4c8cf87f21850efe
probably-kit <probably.kit@gmail.com>
2024-01-08 16:12:44 +0100

    replace files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/main.css';
 
 function createTabNavigation() {
   const tabs = { home: homeTab, menu: menuTab, contact: contactTab };
-  const content = document.getElementById('content');
+  const content = gitdocument.getElementById('content');
 
   for (let tab in tabs) {
     const btn = document.createElement('button');
