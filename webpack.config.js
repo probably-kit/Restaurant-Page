bafc058a21793d0cec02f79a44e3ae70b43d5421
probably-kit <probably.kit@gmail.com>
2024-01-08 16:41:51 +0100

    replace files

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: 'probably-kit/Restaurant-Page' 
   },
   module: {
     rules: [
