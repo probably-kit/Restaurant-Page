d57283e7f940d4a1d89c0dfd9d3f5241c7b6d55a
probably-kit <probably.kit@gmail.com>
2024-01-08 17:44:19 +0100

    fix bugs and deploy gitpages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'probably-kit/Restaurant-Page' 
+    publicPath: 'Restaurant-Page' 
   },
   module: {
     rules: [
